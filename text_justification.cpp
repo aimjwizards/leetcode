efc9977404811a331e6b21ccbd2b6ec01f24afba
Jing Wu <aimjwizards@gmail.com>
2014-06-22 08:13:10 -0400

    https://oj.leetcode.com/problems/text-justification/

--- text_justification.cpp
+++ text_justification.cpp
@@ -1,5 +1,6 @@
     void process(vector<string> &words, vector<string>&result, int start, int end, int L) {
         int size = 0;
+        // end of the setence, left justification
         if(end==words.size()-1 ) {
             string s;
             int last_space = L;
@@ -15,27 +16,27 @@
             }
             result.push_back(s); return;
         }
-
+        // find the total size of the words
         for(int i=start; i<=end; i++) {
             size+=words[i].size();
         }
+        // find the total space size
         int space = L-size;
+        // if it's just one word, left justification
         if(start==end) {
             string s = words[start];
             s+= string((L-size), ' ');
             result.push_back(s); return;
         }
+        // get the average space - n words needs (n-1) consecutive space
         int e_space = space/(end-start);
+        // get the leftover space for the first kth words
         int n=space-e_space*(end-start);
         string s;
         for(int i=start; i<=end; i++) {
-            s+= words[i];
-            if(i!=end) {
-                s+=string(e_space, ' ');
-            }
-            if(n>0) {
-                s+=string(1, ' '); n--;
-            }
+            s+= words[i];                       // word
+            if(i!=end) s+=string(e_space, ' '); // space
+            if((n--)>0) s+=string(1, ' ');      // additional space if any
         }
         result.push_back(s);
     }
@@ -47,9 +48,6 @@
         for(int i=0; i<words.size(); i++) {
             if(i+1<words.size() && (sofar+words[i].size()+words[i+1].size()<L)) {
                 sofar = sofar+ words[i].size()+1;
-            }else if(i+1<words.size() && (sofar+words[i].size()+words[i+1].size()==L)) {
-                process(words, result, start, i, L);
-                start = i+1; sofar = 0; 
             }else{
                 process(words, result, start, i, L);
                 start = i+1; sofar = 0;
@@ -57,6 +55,4 @@
         }
 
         return result;
-    }     
-
-
+    }
